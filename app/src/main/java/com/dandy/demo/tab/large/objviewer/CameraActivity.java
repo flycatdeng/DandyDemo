b663029cb7f3b053176f6cdcb7efee692e240acb
flycatdeng <dengchukun@qq.com>
2018-01-01 22:57:04 +0800

    Camera: 添加CameraActivity空白页面

---
+++ app/src/main/java/com/dandy/demo/tab/large/objviewer/CameraActivity.java
@@ -0,0 +1,15 @@
+package com.dandy.demo.tab.large.objviewer;
+
+import com.dandy.demo.tab.BaseDemoAty;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Created by QueenJar
+ * Wechat: queenjar
+ * Emial: queenjar@qq.com
+ */
+
+public class CameraActivity extends BaseDemoAty {
+}
